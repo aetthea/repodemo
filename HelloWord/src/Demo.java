e99d37a0d65ac1bdb8c1b89fd50e987709285aa6
aetthea71 <aetthea@gmail.com>
2014-12-12 16:33:19 +0700

    Commit kesatu

---
+++ HelloWord/src/Demo.java
@@ -0,0 +1,24 @@
+// Class Demo untuk mensimulasikan Entry Point Program
+// Tambah Komentar baru
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int xx, yy;
+		xx=10; yy=20;
+        System.out.println("Jumlah xx + yy : "+ sum(xx,yy));
+	}
+	
+	public static int sum(int x, int y){		
+		return (x+y);
+	}
+	
+	public static int mak(int x, int y){
+		if (x > y) {
+			return x;
+		}
+		else
+			return y;
+	}
+
+}
